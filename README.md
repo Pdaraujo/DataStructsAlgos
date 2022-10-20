28dac18ff98de3cb14672dc8c080db4b681b277c
Diogo Araújo <diogo.araujo@farfetch.com>
2022-10-20 10:11:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DataStructuresAndAlgos
+
+A description of this package.
---
+++ Sources/DataStructuresAndAlgos/DataStructuresAndAlgos.swift
@@ -0,0 +1,6 @@
+public struct DataStructuresAndAlgos {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
